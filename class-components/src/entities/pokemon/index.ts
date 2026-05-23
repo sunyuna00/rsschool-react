db2a38887fae03a04782919a590c8a686e3f1fc1
Bakasova Fatima <itxtoyif@gmail.com>
2026-05-24 00:59:31 +0600

    style: redesign pokemon details sidebar ui and animations

--- class-components/src/entities/pokemon/index.ts
+++ class-components/src/entities/pokemon/index.ts
@@ -1,4 +1,4 @@
 export * from './api/fetchPokemon';
 export * from './model/types';
 export * from './ui/card';
-export * from './ui/pokemon-details';
+export * from './ui/pokemon-details/pokemon-details';
